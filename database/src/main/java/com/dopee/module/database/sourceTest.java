6447a970059434067e6c52449cc1b1d74194854e
hosung-gn <hosung.han@genesisnest.com>
2024-08-09 15:42:45 +0900

    fix: fix test

--- database/src/main/java/com/dopee/module/database/sourceTest.java
+++ database/src/main/java/com/dopee/module/database/sourceTest.java
@@ -3,6 +3,6 @@ package com.dopee.module.database;
 public class sourceTest {
 
     public static void Hello(){
-        System.out.println("Hello World");
+        System.out.println("Changed Hello World");
     }
 }
